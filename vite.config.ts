e7a51fb99155231a197d88ad33aec963b74c7cd5
smanx <smanx@github.com>
2026-05-03 13:45:33 +0000

    Update - 2026-05-03 13:45:33

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ command, mode }) => {
   const isDev = mode === 'development'
 
   return {
+    base: './',
     plugins: [
       vue(),
       ...(isDev ? [removeManifestPlugin()] : []),
@@ -27,7 +28,7 @@ export default defineConfig(({ command, mode }) => {
           theme_color: '#1f2937',
           background_color: '#f3f4f6',
           display: 'standalone',
-          start_url: '/',
+          start_url: './',
           icons: [
             {
               src: 'pwa-192x192.png',
