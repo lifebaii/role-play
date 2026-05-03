65162b0341d348d1cd2b47b92f9347cdcf283be1
smanx <smanx@github.com>
2026-05-03 15:59:25 +0000

    Update - 2026-05-03 15:59:25

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import { useUserStore } from '@/stores/user'
 import { useAdminStore } from '@/stores/admin'
 
@@ -81,7 +81,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
