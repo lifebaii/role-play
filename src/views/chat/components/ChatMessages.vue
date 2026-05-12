86a927077cddb41b3e5fe0ac0f3f4f69b8424d03
yuki-embedded <yuki-embedded@users.noreply.github.com>
2026-05-12 02:07:22 +0000

    Update - 2026-05-12 02:07:22

--- src/views/chat/components/ChatMessages.vue
+++ src/views/chat/components/ChatMessages.vue
@@ -11,7 +11,7 @@
       <ChatMessage
         v-for="(message, index) in messages"
         :key="message.id"
-        v-memo="[message.content, message.id, editingIndex === index, index === messages.length - 1 && chatStore.isStreaming]"
+        v-memo="[message.content, message.id, editingIndex === index, index === messages.length - 1 && chatStore.isStreaming, chatStore.currentWaitTime]"
         :message="message"
         :index="index"
         :messages="messages"
